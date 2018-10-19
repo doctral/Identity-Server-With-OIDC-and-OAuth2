28fccb7af59014fde91fdc4d07547a4fdbc66529
doctral <lifei.8886196@gmail.com>
2018-10-18 23:55:21 -0400

    setup identity server architecture

---
+++ IdentityServer.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IdentityServer.Application
+{
+	public class Class1
+	{
+	}
+}
---
+++ IdentityServer.IoC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IdentityServer.IoC
+{
+	public class Class1
+	{
+	}
+}
